8aba364226ad7ddd7d029f22cd142b540170e9e4
Nico Auchli <nico.auchli@fhnw.ch>
2024-11-08 14:21:38 +0100

    dockefile added and .env for database

---
+++ dockerfile
@@ -0,0 +1,21 @@
+# Verwende ein Node-Image als Basis für die Backend-Anwendung
+FROM node:16-alpine
+
+# Arbeitsverzeichnis erstellen und setzen
+WORKDIR /app
+
+# Package.json und Package-lock.json kopieren und Abhängigkeiten installieren
+COPY package*.json ./
+RUN npm install
+
+# Den gesamten Source-Code kopieren
+COPY . .
+
+# Nest.js-Anwendung bauen
+RUN npm run build
+
+# Der Port, auf dem das Backend läuft (standardmäßig 3000)
+EXPOSE 3000
+
+# Starten der Nest.js-Anwendung
+CMD ["npm", "run", "start:prod"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,26 @@ import { TodosModule } from './todos/todos.module';
 import { Todo } from './todos/entities/todo.entity';
 import { EmployeeToTodoModule } from './employee-to-todo/employee-to-todo.module';
 import { EmployeeToTodo } from './employee-to-todo/entities/employee-to-todo.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    EmployeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'monorail.proxy.rlwy.net',
-      port: 15437,
-      username: 'postgres',
-      password: 'wRWSQnOQgxyUCKaxaLmTjaaEsYWtQTrA',
-      database: 'railway',
-      entities: [Employee, Todo, EmployeeToTodo],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }), // Lädt die Umgebungsvariablen aus der .env-Datei
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: parseInt(configService.get<string>('DATABASE_PORT'), 10),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Employee, Todo, EmployeeToTodo],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
+    EmployeesModule,
     TodosModule,
     EmployeeToTodoModule,
   ],
