28ab5b2796d935c57f6706155351f0b19da4d714
Nico Auchli <nico.auchli@students.fhnw.ch>
2024-11-11 18:45:36 +0100

    minor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as process from "node:process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,6 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
   });
 
-  await app.listen(3000);
+  await app.listen( process.env.PORT || 3000);
 }
 bootstrap();
